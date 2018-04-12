c0605430407faa6fccec9b59b028ef012c3722dc
ChineseBoyP <15706035966@163.com>
2018-04-12 19:10:49 +0800

    login seo

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
-console.log('hello')
+$(function(){
+
+
+  // 处理轮播
+  function handleSwiper(){
+    new Swiper ('.swiper-container', {
+      loop: true,
+      // 如果需要分页器
+      pagination: {
+        el: '.swiper-pagination',
+      }
+    })    
+  }
+  // 页面初始化完成之后，触发该事件
+  $(document).on("pageInit", function(e, pageId, $page) {
+    // 处理轮播效果
+    handleSwiper();
+  });
+  $.init();
+});
